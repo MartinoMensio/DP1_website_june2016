0be9e5607e06745ccd26e7e69e37587e7416ea67
Martino Mensio <martinomensio@outlook.it>
2016-05-30 19:43:54 +0200

    test database

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-phpinfo();
+  $host = "us-cdbr-azure-west-c.cloudapp.net";
+  $user = "b411bdc8084ca4";
+  $pwd = "fdd4ffbb";
+  $db = "dp-web-jun16-martinomensio";
+  // Connect to database.
+  try {
+    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  }
+  catch(Exception $e){
+    die(var_dump($e));
+  }
+  echo 'successfully connected to db';
 ?>
