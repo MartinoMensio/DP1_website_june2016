c67ad1b3699f7a46cdd8cf0239eb65d91266fd0f
Martino Mensio <martinomensio@outlook.it>
2016-05-30 20:32:17 +0200

    test database 2

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
   $pwd = "fdd4ffbb";
   $db = "dp-web-jun16-martinomensio";
   // Connect to database.
-  try {
-    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  }
-  catch(Exception $e){
-    die(var_dump($e));
+  $conn = new mysqli_connect( $host, $user, $pwd, $db);
+  if(!conn) {
+    die("impossible to connect to database");
   }
   echo 'successfully connected to db';
+  $conn->autocommit(false);
+  $query = $conn->query("SELECT * FROM users");
+  if(!query) {
+    $conn->close();
+    die("query failed");
+  }
+  while($row = $query->fetch_object()) {
+    var_dump($row);
+  }
+  $conn->close();
 ?>
