621c5aeeec7d6b26307322886ae6002f26ca2b09
MartinoMensio <martinomensio@outlook.it>
2016-06-06 11:23:25 +0200

    check cookies

--- config.php
+++ config.php
@@ -7,6 +7,29 @@ if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
 	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], TRUE, 301);
 }
 
+function checkCookies() {
+	//echo sizeof($_REQUEST);
+	if(isset($_REQUEST["testing"])) {
+		// cookie has been set by last visit, verify it
+		if(!isset($_COOKIE["test"])) {
+			// cookies are disabled
+			die('you must enable cookies to view this site');
+		}
+	} else if (!isset($_COOKIE["test"])) {
+		// this is the first visit, or cookies disabled
+		setcookie('test', 'hello');
+		if (sizeof($_REQUEST)) {
+			// add a new argument
+			header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "&testing", TRUE, 301);
+		} else {
+			// this is the only argument
+			header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . "?testing", TRUE, 301);
+		}
+	}
+}
+
+checkCookies();
+
 $maxInactiveTime = 60 * 2;
 $numberOfMachines = 4;
 
